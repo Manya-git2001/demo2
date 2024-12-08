90b7bd3ce78a8dbf121e2d7ce536047bdecfa722
Manya-git2001 <manya.mishra@card91.io>
2024-12-08 19:54:53 +0530

    first commit

---
+++ src/main/java/com/example/demo/a/First.java
@@ -0,0 +1,13 @@
+package com.example.demo.a;
+
+
+
+public class First {
+
+    public static void First(){
+        System.out.println("master");
+        System.out.println("First Branch");
+    }
+
+
+}
